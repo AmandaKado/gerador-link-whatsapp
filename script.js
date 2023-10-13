9a40cf31b2c1ed0639ddd1111df5adffa6a8b733
Amanda Kado <105023810+AmandaKado@users.noreply.github.com>
2023-10-13 11:26:48 -0300

    tentando novamente

--- script.js
+++ script.js
@@ -20,24 +20,17 @@ function copiarLinkParaAreaDeTransferencia(link) {
     document.body.removeChild(input);
 }
 
-function codificarEmoji(emoji) {
-    return encodeURIComponent(emoji);
-}
-
 // Função para gerar o link do WhatsApp com mensagem personalizada e copiá-lo
 function gerarLinkWhatsApp() {
     const numero = document.getElementById("numero").value;
     const nome = document.getElementById("nome").value;
     const linkGerado = document.getElementById("linkGerado");
-    const emoji = '👋';
 
     if (numero) {
         // Formate o número removendo caracteres não numéricos
         const numeroFormatado = numero.replace(/\D/g, "");
-        const emojiCodificado = codificarEmoji(emoji);
-
         // Monte o link com a mensagem
-        const link = `https://wa.me/${numeroFormatado}?text=Hello%20${nome}%2C%20how%20are%20you%3F%20Patricia%20here%20%${emojiCodificado}`;
+        const link = `https://wa.me/${numeroFormatado}?text=Hello%20${nome}%2C%20how%20are%20you%3F%20Patricia%20from%20Click%20Millionaire%20here!%20`;
         linkGerado.innerHTML = `<a href="${link}" target="_blank">${link}</a>`;
 
         // Copie o link para a área de transferência
